3d1934ba278183591464ece1e735ef62717d4140
gewl <gilbertwlawson@gmail.com>
2017-04-28 12:28:04 -0400

    Cleaned/standardized output

--- MultisourceDijkstra/Program.cs
+++ MultisourceDijkstra/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// This is an implementation of Dijkstra's Algorithm in C# that solves the multisource shortest-paths problem
+// on edge-weighted digraphs with positive edge-weights; given an edge-weighted digraph and a set of sources,
+// it will return the shortest path to each vertex from any source.
+// This implementation reads the EWD from a text file in the format used in Sedgewick's "Algorithms"—examples
+// at http://algs4.cs.princeton.edu/44sp/tinyEWD.txt, http://algs4.cs.princeton.edu/44sp/mediumEWD.txt and
+// http://algs4.cs.princeton.edu/44sp/largeEWD.txt. It then prompts the user to list sources, and then writes
+// the shortest-paths forest to the console.
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -9,18 +16,13 @@ namespace MultisourceDijkstra
     class MultisourceDijkstra
     {
         static EdgeWeightedDigraph ewd;
-        // Pseudo-priority-queue that stores vertices sorted on distance from source
-        // Key: distance (float)
-        // Value: list of vertices found at that distance (List<int>)
-        // Use: While loop during pathfinding process dequeues lowest vertex at shortest distance, adds to tree, and queues eligible edges from that vertex
-        static SortedDictionary<float, List<int>> verticesPriorityQueue;
-        // Stores distances to vertices in `vertices` sortedDict for easy reference
-        // Key: vertex (int)
-        // Value: distance (float)
-        static Dictionary<int, float> verticesQueueRef;
+        // Acts as priority queue for eligible edges
+        static SortedSet<Vertex> vertices;
         
         // stores distance to vertex n from source s at distanceTo[n] in shortest-paths tree (SPT)
         static float[] distanceTo;
+        // stores weight of last edge for printing purposes
+        static float[] lastWeight;
         // stores reference to vertex at head of edge for which vertex n is the tail at edgeTo[n] in SPT
         static int[] edgeTo;
         // sources for SPT
@@ -34,10 +36,10 @@ namespace MultisourceDijkstra
             int vCount = ewd.vertexCount;
 
             distanceTo = new float[vCount];
+            lastWeight = new float[vCount];
             edgeTo = new int[vCount];
 
-            verticesPriorityQueue = new SortedDictionary<float, List<int>>();
-            verticesQueueRef = new Dictionary<int, float>();
+            vertices = new SortedSet<Vertex>(new VertexComparer());
 
             Console.WriteLine("Which nodes between 0 and " + (vCount - 1) + " is/are the source(s)? (Format: Integers separate by spaces, e.g. `2 4 5 7`)");
             string sText = Console.ReadLine();
@@ -48,6 +50,7 @@ namespace MultisourceDijkstra
                 if (s.Contains<int>(v))
                 {
                     distanceTo[v] = 0.0f;
+                    queueVertex(v);
                 }
                 else
                 {
@@ -55,18 +58,10 @@ namespace MultisourceDijkstra
                 }
             }
 
-            // add each source vertex to sortedDictionary with distance of 0.0f
-            foreach (int source in s)
+            while (vertices.Count != 0)
             {
-                Console.WriteLine(source);
-                queueVertex(source);
-            }
-
-            // while sortedDictionary has vertices, remove closest vertex, add to tree, and add edges from that vertex to sortedDictionary
-            while (verticesPriorityQueue.Count != 0)
-            {
-                int nextVertex = verticesPriorityQueue.Values.First().First<int>();
-                removeVertex(nextVertex);
+                Vertex nextVertex = vertices.First();
+                vertices.Remove(nextVertex);
                 relax(ewd, nextVertex);
             }
 
@@ -75,31 +70,11 @@ namespace MultisourceDijkstra
             Console.ReadKey();
         }
 
-        // places vertex into `verticesPriorityQueue` sortedDict and `verticesQueueRef` dict
-        private static void queueVertex(int vertex)
+        // Adds vertex to priority queue
+        private static void queueVertex(int vertexId)
         {
-            // if nodes does not contain list for nodes at given distance, initializes new list under distance key
-            if (!verticesPriorityQueue.ContainsKey(distanceTo[vertex]))
-            {
-                verticesPriorityQueue.Add(distanceTo[vertex], new List<int>());
-            }
-            // adds vertex to list under distance key in sortedDictionary
-            verticesPriorityQueue[distanceTo[vertex]].Add(vertex);
-            // adds distance to sortedDictionary under vertex key
-            verticesQueueRef.Add(vertex, distanceTo[vertex]);
-        }
-
-        // removes vertex from priority queue & reference dict
-        private static void removeVertex(int vertex)
-        {
-            float nodeRef = verticesQueueRef[vertex];
-            verticesPriorityQueue[verticesQueueRef[vertex]].Remove(vertex);
-            // if vertex being removed is last under 'distance' key in sortedDict, delete the key/value in sortedDict to help with garbage collection
-            if (verticesPriorityQueue[verticesQueueRef[vertex]].Count == 0)
-            {
-                verticesPriorityQueue.Remove(verticesQueueRef[vertex]);
-            }
-            verticesQueueRef.Remove(vertex);
+            Vertex newVertex = new Vertex(vertexId, distanceTo[vertexId]);
+            vertices.Add(newVertex);
         }
 
         private static int[] parseSources(string input)
@@ -124,23 +99,22 @@ namespace MultisourceDijkstra
             }
         }
 
-        // Relax edge to nearest-distance node from pseudo-priority-queue `nodes`
-        // Add edges to `nodes` (and references to `nodesRefs`)
-        private static void relax(EdgeWeightedDigraph graph, int vertex)
+        private static void relax(EdgeWeightedDigraph graph, Vertex vertex)
         {
-            List<int> tails = graph.adj(vertex);
+            int vertexId = vertex.id;
+            List<int> tails = graph.adj(vertexId);
 
+            // Adds eligible edges to priority queue
             foreach (int tail in tails)
             {
-                float weight = graph.getWeight(vertex, tail);
-                if (distanceTo[tail] > distanceTo[vertex] + weight)
+                float weight = graph.getWeight(vertexId, tail);
+                if (distanceTo[tail] > distanceTo[vertexId] + weight)
                 {
-                    distanceTo[tail] = distanceTo[vertex] + weight;
-                    edgeTo[tail] = vertex;
-                    if (verticesQueueRef.ContainsKey(tail))
-                    {
-                        removeVertex(tail);
-                    }
+                    distanceTo[tail] = distanceTo[vertexId] + weight;
+                    lastWeight[tail] = weight;
+                    edgeTo[tail] = vertexId;
+                    // If vertex already on priority queue, its entry there has a greater weight and so is obsolete;
+                    vertices.RemoveWhere(v => v.id == vertexId);
                     queueVertex(tail);
                 }
             }
@@ -148,19 +122,47 @@ namespace MultisourceDijkstra
 
         public static void printSPT()
         {
+            //Console.WriteLine("\nVertex\tEdge\tEdge Weight\tTotal Weight\n");
+            Console.WriteLine("\n{0, -8} {1, -20} {2, -20} {3, -20}\n", "Vertex", "Edge", "Edge Weight", "Total Weight");
             for (int i = 0; i < distanceTo.Length; i++)
             {
-                Console.WriteLine("Vertex " + i + ":");
                 if (s.Contains<int>(i))
                 {
-                    Console.WriteLine("Source vertex.");
+                    Console.WriteLine("{0, -8} {1, -20} {2, -20} {3, -20}", i, "Source", "0.0", "0.0");
                 }
                 else
                 {
-                    Console.WriteLine("Edge from " + edgeTo[i] + ".");
-                    Console.WriteLine("Distance from nearest source to " + i + ": " + distanceTo[i]);
+                    Console.WriteLine("{0, -8} {1, -20} {2, -20} {3, -20}", i, edgeTo[i] + "->" + i, lastWeight[i], distanceTo[i]);
                 }
             }
+        }
+    }
+
+    // default class to hold vertices in priority queue
+    public class Vertex
+    {
+        public int id { get; protected set; }
+        public float weight { get; protected set; }
+        public Vertex(int vertexID, float vertexWeight)
+        {
+            id = vertexID;
+            weight = vertexWeight;
+        }
+    }
+
+    // used by SortedSet `vertices` to sort contents; primarily by weight/distance, secondarily by int vertexId
+    internal class VertexComparer : IComparer<Vertex>
+    {
+        public int Compare (Vertex x, Vertex y)
+        {
+            int result = x.weight.CompareTo(y.weight);
+
+            if (result == 0)
+            {
+                result = x.id.CompareTo(y.id);
+            }
+
+            return result;
         }
     }
 
